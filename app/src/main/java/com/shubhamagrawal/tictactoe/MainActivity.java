0fac4f0eeafb17b958285b02dfb53067d95b71a0
shubhamagrawal6 <shubhamagrawal322@gmail.com>
2022-01-14 00:55:37 +0530

    homepage and player setup page

--- app/src/main/java/com/shubhamagrawal/tictactoe/MainActivity.java
+++ app/src/main/java/com/shubhamagrawal/tictactoe/MainActivity.java
@@ -2,7 +2,9 @@ package com.shubhamagrawal.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void playButtonClick(View view){
+        Intent intent = new Intent(this, PlayerSetup.class);
+        startActivity(intent);
+
     }
 }
